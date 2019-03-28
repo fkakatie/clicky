83d6ca91c895a01826776334a355722ec2406cf5
fkakatie <sextonskatie@gmail.com>
2019-03-27 23:15:38 -0600

    Updates

---
+++ precache-manifest.decc554533015eabad10f1fc450b0454.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  },
+  {
+    "revision": "735835c0d4c072799f94",
+    "url": "/clicky/static/js/main.749c9888.chunk.js"
+  },
+  {
+    "revision": "63581d89cd18fccee25a",
+    "url": "/clicky/static/js/2.deadf491.chunk.js"
+  },
+  {
+    "revision": "735835c0d4c072799f94",
+    "url": "/clicky/static/css/main.0048fbd5.chunk.css"
+  },
+  {
+    "revision": "b96ad8fc401800260bf9cb3609fe60de",
+    "url": "/clicky/index.html"
+  }
+];
